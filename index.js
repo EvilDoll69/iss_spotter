3a23232345cba4ab1ad992904aa6dc6a99f2b2dd
EvilDoll69 <tetiana.fediushyn@gmail.com>
2021-12-03 20:40:35 +0000

    Geolocation

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
+
+
+fetchMyIP((error, ip) => {
+  
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP("76.69.65.109", (error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+
+});
--- iss.js
+++ iss.js
@@ -6,8 +6,52 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
- const fetchMyIP = function(callback) { 
-  // use request to fetch IP address from JSON API
-}
 
-module.exports = { fetchMyIP };
+const request = require('request');
+
+
+const fetchMyIP = function(callback) {
+  const url = 'https://api.ipify.org?format=json';
+
+  request(url, (error, response, body) => {
+    
+    if (error) {
+      return callback(error, null);
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);//new Error object that we can pass around. In this case, 
+      // we pass it back to the callback to indicate that something went wrong
+      return;
+    }
+    const ip = JSON.parse(body).ip;//.ip make a string
+    callback(null,ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  
+  request(`https://api.freegeoip.app/json/${ip}?apikey=31508530-5472-11ec-9ae1-9f20f199f4af`, (error, response, body) => {
+
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);//new Error object that we can pass around. In this case, 
+      // we pass it back to the callback to indicate that something went wrong
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    return callback(null, { latitude, longitude });
+  });
+      
+  };
+
+ 
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+};
